c096d180ee0f7debb33cd518fe7b98eae72bc7c1
Nguyen Dinh Truong <truongnd1124@gmail.com>
2025-10-17 19:41:20 +0700

    create api ultilities

---
+++ api/sign_out.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+// Xoá tất cả biến session
+$_SESSION = [];
+
+// Huỷ session cookie nếu có
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Huỷ session
+session_destroy();
+
+// Chuyển hướng về trang đăng nhập hoặc home
+header("Location: ../views/form/form.php"); // sửa đường dẫn theo project của bạn
+exit;
+?>
