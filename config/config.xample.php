bb9ce67728b4939ef9042f9caca00418fa3e3c3c
Nguyen Dinh Truong <truongnd1124@gmail.com>
2025-10-29 18:25:05 +0700

    update config.xample.php and remove junk files

--- config/config.xample.php
+++ config/config.xample.php
@@ -3,4 +3,6 @@
 define('GITHUB_CLIENT_ID', 'Paste_token_here');
 define('GITHUB_CLIENT_SECRET', 'Paste_your_github_secret_token_here');
 define('GITHUB_REDIRECT_URI', 'Paste_your_link_redirect_here');
+define('AUTH_EMAIL','your_app_email');
+define('AUTH_PASSWORD','password_email');
 ?>
